f6cf39467ad36776f8246c7f48c28965e4089faf
reidlum <reidlum@hawaii.edu>
2022-05-13 09:54:36 -1000

    Class files

---
+++ Rectangle.h
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file Rectangle.h
+/// @version 1.0
+///
+/// @author Reid Lum <reidlum@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+
+class Rectangle {
+
+};
+
---
+++ Square.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file Square.h
+/// @version 1.0
+///
+/// @author Reid Lum <reidlum@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+
+class Square {
+
+};
+
+
