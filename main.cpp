e2883f4d752ad52f53fb338250953b3b96c5bc3c
reidlum <reidlum@hawaii.edu>
2022-05-13 09:55:42 -1000

    Main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-
+/*
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Just making sure!" << std::endl;
+
+    return 0;
+}
+*/
+// Main for Classes
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Square mySquare(2);
+    std::cout << "The area of mySquare is: "
+              << mySquare.compute_area()
+              << std::endl;
+
+    Rectangle myRectangle( 2, 4 );
+    std::cout << "The area of myRectangle is: "
+              << myRectangle.compute_area()
+              << std::endl;
 
     return 0;
 }
