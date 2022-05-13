fe824a68aa6e356dcbcb1b9cfa9ab8d3762d86a4
reidlum <reidlum@hawaii.edu>
2022-05-13 10:31:28 -1000

    commented tests and made header

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Reid Lum <reidlum@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include "Square.h"
 
@@ -16,24 +26,26 @@ int main() {
               << std::endl;
 
     //My tests
-
-    // testing getSide()
-    /*
+    /* testing getSide() ///////////////////////////
     std::cout << mySquare.getSide() << std::endl;
     */
-    /* tests a negative side
+    /* testing getLength() and getWidth() //////////
+    std::cout << myRectangle.getLength() << std::endl;
+    std::cout << myRectangle.getWidth() << std::endl;
+    */
+    /* tests a negative side ///////////////////////
     Square mySquare2(-1);
     std::cout << "The area of mySquare is: "
               << mySquare2.compute_area()
               << std::endl;
     */
-    /*
+    /* tests 0 side ////////////////////////////////
     Square mySquare3(0);
     std::cout << "The area of mySquare is: "
               << mySquare3.compute_area()
               << std::endl;
     */
-    /* testing negative rectangle
+    /* testing negative rectangle //////////////////
     Rectangle myRectangle2( -2, 4 );
     std::cout << "The area of myRectangle is: "
               << myRectangle2.compute_area()
